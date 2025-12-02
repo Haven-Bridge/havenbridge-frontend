0ebdb94fe5782692a807f03659933511b1548715
Ebi-Tech <difebi14@gmail.com>
2025-12-02 22:49:32 +0200

    Added "Home" nav link

--- havenbridge-frontend/src/components/Navbar.tsx
+++ havenbridge-frontend/src/components/Navbar.tsx
@@ -16,6 +16,7 @@ export default function Navbar() {
   }, []);
 
   const navLinks = [
+    { name: 'Home', href: '/' },
     { name: 'Services', href: '/services' },
     { name: 'Projects', href: '/projects' },
     { name: 'About', href: '/about' },
